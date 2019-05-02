3cb3326d0df3d24066bf42c037d7a759b0c486c4
Nisheet Sharma <nisheethsharma@gmail.com>
2019-05-02 15:42:47 +0530

    side-menu actions done

--- app/dashboard/side-menu/side-menu.controller.js
+++ app/dashboard/side-menu/side-menu.controller.js
@@ -2,39 +2,60 @@
 
     function SideMenuController($scope) {
         $scope.menu = {
-            activeItem: 'Dashboard'
+            items: [
+                {
+                    listIcon: 'dashboard',
+                    title: 'Dashboard',
+                    listActionIcon: 'arrow_drop_down',
+                    subMenu: {
+                        show: true,
+                        items: [
+                            { title: 'Page Visitors' },
+                            { title: 'Post Performance' },
+                            { title: 'Team Overall' },
+                        ]
+                    }
+                },
+                {
+                    listIcon: 'calendar_today',
+                    title: 'Calendar'
+                },
+                {
+                    listIcon: 'inbox',
+                    title: 'Inbox',
+                    listActionIcon: 'unfold_more'
+                },
+                {
+                    listIcon: 'money',
+                    title: 'Invoicing'
+                },
+                {
+                    listIcon: 'report',
+                    title: 'Lab / Experimental'
+                }
+            ]
         };
-        $scope.menu.items = [
-            {
-                listIcon: 'dashboard',
-                title: 'Dashboard',
-                listActionIcon: 'arrow_drop_down',
-                subMenuItems: [
-                    { title: 'Page Visitors' },
-                    { title: 'Post Performance' },
-                    { title: 'Team Overall' },
-                ]
-            },
-            {
-                listIcon: 'calendar_today',
-                title: 'Calendar'
-            },
-            {
-                listIcon: 'inbox',
-                title: 'Inbox',
-                listActionIcon: 'unfold_more'
-            },
-            {
-                listIcon: 'money',
-                title: 'Invoicing'
-            },
-            {
-                listIcon: 'report',
-                title: 'Lab / Experimental'
-            }
-        ];
+        $scope.menu.activeItem = $scope.menu.items[0];
+        $scope.menu.activeItem.subMenu.activeItem = $scope.menu.activeItem.subMenu.items[0];
+
         $scope.onMenuItemClick = function (item) {
-            $scope.menu.activeItem = item;
+            if ($scope.menu.activeItem.title != item.title) {
+                if ($scope.menu.activeItem.subMenu) {
+                    $scope.menu.activeItem.subMenu.show = false;
+                    $scope.menu.activeItem.subMenu.activeItem = null;
+                }
+                if (item.subMenu) {
+                    item.subMenu.show = true;
+                }
+                return $scope.menu.activeItem = item;
+            }
+            if ($scope.menu.activeItem.subMenu) {
+                $scope.menu.activeItem.subMenu.show = !$scope.menu.activeItem.subMenu.show;
+            }
+        };
+
+        $scope.onSubMenuItemClick = function (item, subItem) {
+            item.subMenu.activeItem = subItem;
         };
     }
 
