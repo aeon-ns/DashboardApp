d0975a54faa43d8c9e9cb3817e831da9f9cd2c1a
Nisheet Sharma <nisheethsharma@gmail.com>
2019-05-02 17:21:18 +0530

    header, chart updates

--- app/dashboard/content/page-visitors/page-visitors.controller.js
+++ app/dashboard/content/page-visitors/page-visitors.controller.js
@@ -1,7 +1,42 @@
-(function() {
+(function () {
 
     function PageVisitorsController($scope) {
-
+        var myChart = document.getElementById('myChart');
+        var ctx = myChart.getContext('2d');
+        var myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                title: 'Page Views',
+                labels: ['Item 1', 'Item 2', 'Item 3'],
+                datasets: [{
+                    data: [10, 20, 30],
+                    backgroundColor: '#4100CE'
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true,
+                            // display: false
+                        },
+                        barThickness: 0,
+                        position: 'right'
+                    }],
+                    xAxes: [{
+                        gridLines: {
+                            display: false
+                        }
+                    }],
+                },
+                responsive: true,
+                backgroundColor: 'white',
+                maintainAspectRatio: true,
+                legend: {
+                    display:false
+                }
+            }
+        });
     }
 
     PageVisitorsController.$inject = ['$scope'];
