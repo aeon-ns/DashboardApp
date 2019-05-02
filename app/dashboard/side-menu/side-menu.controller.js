d27e7882eb139df25426e02866fd2a71f1070b6c
Nisheet Sharma <nisheethsharma@gmail.com>
2019-05-02 14:00:21 +0530

    side menu updates

--- app/dashboard/side-menu/side-menu.controller.js
+++ app/dashboard/side-menu/side-menu.controller.js
@@ -1,7 +1,41 @@
-(function() {
+(function () {
 
     function SideMenuController($scope) {
-
+        $scope.menu = {
+            activeItem: 'Dashboard'
+        };
+        $scope.menu.items = [
+            {
+                listIcon: 'dashboard',
+                title: 'Dashboard',
+                listActionIcon: 'arrow_drop_down',
+                subMenuItems: [
+                    { title: 'Page Visitors' },
+                    { title: 'Post Performance' },
+                    { title: 'Team Overall' },
+                ]
+            },
+            {
+                listIcon: 'calendar_today',
+                title: 'Calendar'
+            },
+            {
+                listIcon: 'inbox',
+                title: 'Inbox',
+                listActionIcon: 'unfold_more'
+            },
+            {
+                listIcon: 'money',
+                title: 'Invoicing'
+            },
+            {
+                listIcon: 'report',
+                title: 'Lab / Experimental'
+            }
+        ];
+        $scope.onMenuItemClick = function (item) {
+            $scope.menu.activeItem = item;
+        };
     }
 
     SideMenuController.$inject = ['$scope'];
