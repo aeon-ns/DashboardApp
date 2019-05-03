891df7a241d29fe616d1557101925d6c86df4e62
Nisheet Sharma <nisheethsharma@gmail.com>
2019-05-03 11:55:50 +0530

    chart updates

--- app/dashboard/content/page-visitors/page-visitors.controller.js
+++ app/dashboard/content/page-visitors/page-visitors.controller.js
@@ -1,15 +1,24 @@
 (function () {
 
     function PageVisitorsController($scope) {
-        var myChart = document.getElementById('myChart');
-        var ctx = myChart.getContext('2d');
-        var myChart = new Chart(ctx, {
+
+        let days = [];
+        let views = [];
+        for (let i = 0; i < 30; i++) {
+            days[i] = i + 1;
+            views[i] = Math.floor(1000 + Math.random() * 9000);
+        }
+
+        let myChartElement = document.getElementById('myChart');
+        let ctx = myChartElement.getContext('2d');
+
+        let myChart = new Chart(ctx, {
             type: 'bar',
             data: {
                 title: 'Page Views',
-                labels: ['Item 1', 'Item 2', 'Item 3'],
+                labels: days,
                 datasets: [{
-                    data: [10, 20, 30],
+                    data: views,
                     backgroundColor: '#4100CE'
                 }]
             },
@@ -18,12 +27,23 @@
                     yAxes: [{
                         ticks: {
                             beginAtZero: true,
-                            // display: false
-                        },
-                        barThickness: 0,
-                        position: 'right'
+                            callback: function (value, index, values) {
+                                if (value < 1000) {
+                                    return value;
+                                }
+                                if (value < 1000000) {
+                                    return (value / 1000) + 'K';
+                                }
+                                return (value/1000000)+ 'M';
+                            }
+                        }, 
+                        position: 'right',
+                        gridLines: {
+                            drawBorder: false
+                        }
                     }],
                     xAxes: [{
+                        maxBarThickness: 18,
                         gridLines: {
                             display: false
                         }
@@ -33,8 +53,9 @@
                 backgroundColor: 'white',
                 maintainAspectRatio: true,
                 legend: {
-                    display:false
-                }
+                    display: false
+                },
+                curvature: 1,
             }
         });
     }
