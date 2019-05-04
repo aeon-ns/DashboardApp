4d5c528f6ae7450955ea061ecd52b72432e5f4aa
Nisheet Sharma <nisheethsharma@gmail.com>
2019-05-04 10:29:56 +0530

    binding to env PORT

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 const logger = require('morgan');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(logger('dev'));
 
